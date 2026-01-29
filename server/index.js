012f21fbb186cf41bd628b072595248686e94867
Mansi <Mansi.kharb@kc-one.co>
2026-01-29 15:57:34 +0530

    Fix invalid * route for Express 5

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+app.options('/*', cors());
 app.use(express.json());
 
 const apiRoutes = require('./routes/api');
